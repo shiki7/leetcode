25700ec26ad05cc62702dbb49e0f39feedac6bd4
shiki7 <bitbite8@gmail.com>
2019-06-27 09:15:31 +0900

    Delete p27.py

--- p27.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        l = []
-        for index, item in reversed(list(enumerate(nums))):
-            if item == val:
-                del nums[index]
