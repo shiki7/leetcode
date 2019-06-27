d01662b050ee6319e25ffbb899f624967086648f
shiki7 <bitbite8@gmail.com>
2019-06-27 09:32:45 +0900

    Delete p575.py

--- p575.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def distributeCandies(self, candies: List[int]) -> int:
-        max_kinds = int (len(candies) / 2)
-        uniq_kinds = len(set(candies))
-        if uniq_kinds > max_kinds:
-            return max_kinds
-        else:
-            return uniq_kinds
