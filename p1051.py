94e2b01edc5b9d0d9ee370a53e893a2b1ab2944e
shiki7 <bitbite8@gmail.com>
2019-06-27 09:40:31 +0900

    Delete p1051.py

--- p1051.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def heightChecker(self, heights: List[int]) -> int:
-        sorted_heights = sorted(heights)
-        count = 0
-        for a, b in zip(heights, sorted_heights):
-            if a != b:
-                count += 1
-        return count
