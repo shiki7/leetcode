15ab08ec54b6b47bf1bdafae126e2736905c708b
shiki7 <bitbite8@gmail.com>
2019-06-27 09:49:28 +0900

    Delete p771.py

--- p771.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def numJewelsInStones(self, J: str, S: str) -> int:
-        sum = 0
-        for char_J in J:
-            for char_S in S:
-                if char_J == char_S:
-                    sum += 1
-        return sum
-        
