78d535f820891e5062a890a43431f1fdbab988ec
shiki7 <bitbite8@gmail.com>
2019-06-27 10:29:59 +0900

    Delete p961.py

--- p961.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def repeatedNTimes(self, A: List[int]) -> int:
-        dict = {}
-        for node in A:
-            if node in dict.keys():
-                dict[node] += 1
-            else:
-                dict[node] = 1
-        return max(dict, key=dict.get)
