2dc3b0ddc6af9cd049ef07dd45b25745108fb0f1
shiki7 <bitbite8@gmail.com>
2019-06-27 09:25:47 +0900

    Delete p406.py

--- p406.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        answer = []
-        people.sort(key = lambda x: (-x[0], x[1]))
-        for h, k in people:
-            answer.insert(k, [h, k])
-        return answer
