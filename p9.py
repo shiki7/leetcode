0a5ce58cdb1e806b97953fdbf086126ca2d0b332
shiki7 <bitbite8@gmail.com>
2019-06-27 09:12:31 +0900

    Delete p9.py

--- p9.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        str_x = str(x)
-        str_x_reverse = str_x[::-1]
-        if str_x == str_x_reverse:
-            return True
-        else:
-            return False
