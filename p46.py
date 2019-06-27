8e1a98531cf629b4a4e5e2221543425a8916a5dd
shiki7 <bitbite8@gmail.com>
2019-06-27 09:16:37 +0900

    Delete p46.py

--- p46.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        from itertools import permutations
-        ans = []
-        for i in itertools.permutations(nums):
-            ans += [i]
-        return ans
