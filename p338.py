2ed13849cf24a5c0a003f6431382f87b4c7268ed
shiki7 <bitbite8@gmail.com>
2019-06-27 09:24:01 +0900

    Delete p338.py

--- p338.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def countBits(self, num: int) -> List[int]:
-        result = [0]
-        for i in range(1, num + 1):
-            result += [bin(i)[2:].count("1")]
-        return result
