6c59a04c4ec3c97daec1706f178d4f6ce5ddd5b5
shiki7 <bitbite8@gmail.com>
2020-02-26 19:17:23 +0900

    Delete solution.py

--- 1207: unique-number-of-occurrences/solution.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def uniqueOccurrences(self, arr: List[int]) -> bool:
-        from collections import Counter
-        counter = Counter(arr)
-        return len(set(counter.values())) == len(set(arr))
